a1334e6ffa438957c3413b1dbc1a6c239052c6fe
Thelastpepas <yonatancp.mgvc@gmail.com>
2024-07-30 11:17:19 -0500

    add divide multiply methods

--- calculator/model.py
+++ calculator/model.py
@@ -2,3 +2,15 @@ class Calculator:
 
     def add(self, a: float , b: float ) -> float:
         return a + b
+
+    def subtract(self, a: float , b: float ) -> float:
+        return a - b
+
+    def multiply(self, a: float , b: float ) -> float:
+        return a * b
+
+    def divide(self, a: float , b:float ) -> float | str:
+        if b == 0
+            return "cannot divide by 0!"
+        return a / b
+
